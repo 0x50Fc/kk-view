ad02d3cb923043b807868ffc9f796b565effea5d
wangchao15 <wangchao15@staff.weibo.com>
2018-02-09 14:22:06 +0800

    TextElement简单跑起来

--- app/src/main/java/cn/kkmofang/demo/MainActivity.java
+++ app/src/main/java/cn/kkmofang/demo/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Point;
 import android.os.Bundle;
 
 import cn.kkmofang.view.DocumentView;
+import cn.kkmofang.view.TextElement;
 import cn.kkmofang.view.ViewContext;
 import cn.kkmofang.view.ViewElement;
 import cn.kkmofang.view.value.Pixel;
@@ -32,8 +33,9 @@ public class MainActivity extends Activity {
         root.setAttrs("width","100%","height","100%","background-color","#fff");
 
         {
-            ViewElement element = new ViewElement();
-            element.setAttrs("width","200rpx","height","200rpx","background-color","#f00");
+            TextElement element = new TextElement();
+            element.setAttrs("width","200rpx","height","200rpx","background-color","#f00",
+                    "#text", "nihao");
             root.append(element);
         }
 
